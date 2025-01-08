53120c2b4d7467f61ad4abf4fd8f58710b475bd1
Admin <giggshsu@gmail.com>
2025-01-08 23:47:38 +0800

    Started Photobook Project

---
+++ src/main/java/com/example/myphotobook/PhotobookAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.myphotobook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotobookAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotobookAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myphotobook/PhotobookAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myphotobook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotobookAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
